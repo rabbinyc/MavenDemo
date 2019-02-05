ff044d6d2e611cec1ac9a08ef3e7db318cc4dc9f
rabbinyc@gmail.com <rabbinyc@gmail.com>
2019-02-04 20:30:56 -0500

    Commit for Subtract method

--- src/test/java/com/qa/test/DemoTest.java
+++ src/test/java/com/qa/test/DemoTest.java
@@ -1,72 +1,78 @@
 package com.qa.test;
 
-
 import org.testng.annotations.Test;
 
 import junit.framework.Assert;
 
 public class DemoTest {
 
-	//This is my 2nd commit
-	
+	// This is my 2nd commit
+
 	@Test
-	
+
 	public void sum() {
-		
+
 		System.out.println("Sum");
 		int a = 10;
 		int b = 20;
-		Assert.assertEquals(30, a+b);
-		
+		Assert.assertEquals(30, a + b);
+
 	}
-	
-	
-  @Test
-	
+
+	@Test
+
 	public void subtraction() {
-		
+
 		System.out.println("subtraction");
 		int a = 10;
 		int b = 20;
-		Assert.assertEquals(10, b-a);
-		
+		Assert.assertEquals(10, b - a);
+
 	}
 	
-
-  @Test
-
-public void div() {
 	
-	System.out.println("Div");
-	int a = 10;
-	int b = 20;
-	Assert.assertEquals(2, b/a);
-	
-}
+	@Test
 
-   @Test
+	public void subtract() {
 
-  public void multiply() {
-  	
-  	System.out.println("Multiply");
-  	int a = 10;
-  	int b = 20;
-  	Assert.assertEquals(200, a*b);
-  	
-  }
-	
-	
-   
-   @Test
+		System.out.println("Subtract");
+		int a = 300;
+		int b = 200;
+		Assert.assertEquals(100, a - b);
 
-   public void addition() {
-   	
-   	System.out.println("Addition");
-   	int a = 200;
-   	int b = 200;
-   	Assert.assertEquals(400, a+b);
-   	
-   }
- 	
-	
+	}
+
+	@Test
+
+	public void div() {
+
+		System.out.println("Div");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b / a);
+
+	}
+
+	@Test
+
+	public void multiply() {
+
+		System.out.println("Multiply");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a * b);
+
+	}
+
+	@Test
+
+	public void addition() {
+
+		System.out.println("Addition");
+		int a = 200;
+		int b = 200;
+		Assert.assertEquals(400, a + b);
+
+	}
+
 }
